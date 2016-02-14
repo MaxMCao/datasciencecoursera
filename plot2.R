f00c9a4ce8a6d2506a36a4bab4bd2b6b41217a4b
Meng <a1188216@student.adelaide.edu.au>
2016-02-14 15:02:00 +1100

    Update for the assignment

--- plot2.R
+++ plot2.R
@@ -15,22 +15,9 @@ datetime <- paste(as.Date(mydata$Date), mydata$Time)
 mydata$Datetime <- as.POSIXct(datetime)
 
 # plot in PNG
-png(filename="plto4.png",width=480,height=480,units="px")
-par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
-with(mydata, {
-    plot(Global_active_power ~ Datetime, type = "l",
-    ylab = "Global Active Power", xlab = "")
-    plot(Voltage ~ Datetime, type = "l", ylab = "Voltage", xlab = "datetime")
-    plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Energy sub metering",
-    xlab = "")
-    lines(Sub_metering_2 ~ Datetime, col = 'Red')
-    lines(Sub_metering_3 ~ Datetime, col = 'Blue')
-    legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2,
-    bty = "n",
-    legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-    plot(Global_reactive_power ~ Datetime, type = "l",
-    ylab = "Global_rective_power", xlab = "datetime")
-})
+png(filename="plto2.png",width=480,height=480,units="px")
+plot(mydata$Global_active_power ~ mydata$Datetime, type = "l",
+ylab = "Global Active Power (kilowatts)", xlab = "")
 
 dev.off()
 
